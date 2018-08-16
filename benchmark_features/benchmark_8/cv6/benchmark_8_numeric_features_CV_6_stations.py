087bfff9454474afdfe2441d9470fc5a34c0e51b
LiYanChalmers <li.yan.chalmers@gmail.com>
2018-08-16 22:44:42 +0200

    Test cv 6 on local machine

--- benchmark_features/benchmark_8/cv6/benchmark_8_numeric_features_CV_6_stations.py
+++ benchmark_features/benchmark_8/cv6/benchmark_8_numeric_features_CV_6_stations.py
@@ -9,24 +9,26 @@ from bosch_helper import *
 
 #%% Load data
 x = pd.read_hdf('numeric_b1_b7_nf149.hdf', 'numeric')
-y_train = pd.read_hdf('hpopt_1/numeric_b1_b7_nf149.hdf', 'y_train')
+y_train = pd.read_hdf('numeric_b1_b7_nf149.hdf', 'y_train')
 
 time_station = pd.read_hdf('time_station.hdf', 'time_station')
 
 x = x.join(time_station)
 
 x_train = x.loc['train']
+x_train = x_train.iloc[:, :30]
 x_test = x.loc['test']
+x_test = x_test.iloc[:, :30]
 
 #%% CV
-params = {'max_depth':14, 'eta':0.03, 'silent':1, 'objective':'binary:logistic', 'nthread':20,
+params = {'max_depth':4, 'eta':0.03, 'silent':1, 'objective':'binary:logistic', 'nthread':20,
          'lambda':4, 'subsample':0.9, 'min_child_weight':5, 'booster':'gbtree', 'alpha':0,
          'base_score':0.0058, 'colsample_bytree':0.6}
 
 cv_results, clfs, running_time = \
     cross_val_predict_skf_rm_xgb(params, x_train, y_train, 
-    num_boost_round=80, n_splits=5, 
-    n_repeats=3, random_state=5870577, 
+    num_boost_round=2, n_splits=2, 
+    n_repeats=2, random_state=5870577, 
     verbose_eval=True)
 
 results = {'clfs_cv': clfs, 
@@ -50,14 +52,14 @@ cv_test_std = cv_results['test'].std(axis=1)
 dtrain = xgb.DMatrix(x_train, label=y_train)
 params['seed'] = 587359
 clf = xgb.train(params, dtrain, 
-    num_boost_round=60,
+    num_boost_round=2,
     feval=mcc_eval, 
     evals=[(dtrain, 'train')])
 
 y_train_pred = clf.predict(dtrain)
 
 # Find best threshold 
-thresholds = np.linspace(0.01, 0.99, 400)
+thresholds = np.linspace(0.01, 0.99, 4)
 mcc = np.array([matthews_corrcoef(y_train, y_train_pred>thr) 
     for thr in thresholds])
 #plt.plot(thresholds, mcc)
@@ -75,7 +77,7 @@ dtest = xgb.DMatrix(x_test)
 y_test_pred = clf.predict(dtest)
 y_test_pred_int = (y_test_pred>best_threshold).astype(int)
 
-sub = pd.read_csv("../../data/sample_submission.csv.zip", index_col=0)
+sub = pd.read_csv("sample_submission.csv.zip", index_col=0)
 sub["Response"] = y_test_pred_int
 sub.to_csv("benchmark_8_submission_cv_6_station.csv.gz", compression="gzip")
 
