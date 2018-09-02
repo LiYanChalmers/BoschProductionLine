78bb7d0e2f7ae347bd0e8f92c6fe7b51cd6b900a
LiYanChalmers <li.yan.chalmers@gmail.com>
2018-09-02 21:56:59 +0200

    modify data

---
+++ benchmark_features/hpopt_1/hpop_test_1/ht_40.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#SBATCH -A C3SE2018-1-15
+#SBATCH -p hebbe
+#SBATCH -J ht_40
+#SBATCH -N 1
+#SBATCH -n 20
+#SBATCH -C MEM128
+#SBATCH -t 0-16:0:0
+#SBATCH -o ht_40.stdout
+#SBATCH -e ht_40.stderr
+module purge 
+
+export PATH="/c3se/NOBACKUP/users/lyaa/conda_dir/miniconda/bin:$PATH"
+source activate kaggle
+
+pdcp sample_submission.csv.zip $TMPDIR
+pdcp numeric_b1_b7_nf149.hdf $TMPDIR
+pdcp ht_40.py $TMPDIR
+pdcp bosch_helper.py $TMPDIR
+
+cd $TMPDIR
+
+python ht_40.py
+
+cp *.pickle $SLURM_SUBMIT_DIR
+cp *.gz $SLURM_SUBMIT_DIR
+# End script
